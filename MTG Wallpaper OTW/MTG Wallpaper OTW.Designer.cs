c88daccb59a9bd7bd502b8d1053b22ba7af8b73d
goodtrailer <aldenwu0@gmail.com>
2019-12-19 23:40:39 -0800

    Fetches image, but trouble setting as desktop wallpaper

--- MTG Wallpaper OTW/MTG Wallpaper OTW.Designer.cs
+++ MTG Wallpaper OTW/MTG Wallpaper OTW.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MTG_Wallpaper_OTW
 {
-    partial class Service1
+    partial class MTG_Wallpaper_OTW
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // MTG_Wallpaper_OTW
+            // 
+            this.ServiceName = "MTG Wallpaper OTW";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
         }
 
         #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
     }
 }
--- MTG Wallpaper OTW/Program.cs
+++ MTG Wallpaper OTW/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace MTG_Wallpaper_OTW
 {
@@ -17,7 +12,7 @@ namespace MTG_Wallpaper_OTW
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new MTG_Wallpaper_OTW()
             };
             ServiceBase.Run(ServicesToRun);
         }
