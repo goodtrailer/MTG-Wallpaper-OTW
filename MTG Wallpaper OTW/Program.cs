8988cd68ca8b3883a7f3ff9feda9db97f7ed6d97
goodtrailer <aldenwu0@gmail.com>
2019-12-21 15:15:26 -0800

    polish

--- MTG Wallpaper OTW/Program.cs
+++ MTG Wallpaper OTW/Program.cs
@@ -1,9 +1,9 @@
 using System.Net;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
 using System;
 using Microsoft.Win32.TaskScheduler;
+using System.Diagnostics;
 
 namespace MTG_Wallpaper_OTW
 {
@@ -15,7 +15,6 @@ namespace MTG_Wallpaper_OTW
         const string IMAGE_SUFFIX = "_1920x1080_wallpaper.jpg";
         const string IMAGE_PREFIX = "https://media.magic.wizards.com/images/wallpaper/";
         const string WALLPAPER_PAGE = "https://magic.wizards.com/en/articles/media/wallpapers";
-        const string EXE_NAME = "MTG Wallpaper OTW.exe";
         const string TASK_NAME = "MTG Wallpaper OTW";
         const string DESCRIPTION = "Grabs new weekly Magic: The Gathering wallpaper from the official website and sets it as the desktop wallpaper.";
         const string SETUP_ARG = "setup";
@@ -23,18 +22,22 @@ namespace MTG_Wallpaper_OTW
 
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            try
             {
-                UpdateWallpaper();
-            }
-            else if (args[0].Equals(SETUP_ARG, StringComparison.CurrentCultureIgnoreCase))
-            {
-                SetupTask();
-            }
-            else if (args[0].Equals(REMOVE_ARG, StringComparison.CurrentCultureIgnoreCase))
-            {
-                RemoveTask();
-            }
+                if (args.Length == 0)
+                    UpdateWallpaper();
+                else if (args[0].Equals(SETUP_ARG, StringComparison.CurrentCultureIgnoreCase)
+                        && args.Length == 4
+                        && int.Parse(args[1]) <= 12 && int.Parse(args[1]) > 0
+                        && int.Parse(args[2]) >= 0 && int.Parse(args[2]) < 60
+                        && (args[3].Equals("am", StringComparison.CurrentCultureIgnoreCase)
+                        || args[3].Equals("pm", StringComparison.CurrentCultureIgnoreCase)))
+                    SetupTask(args[1], int.Parse(args[2]), args[3]);
+                else if (args[0].Equals(SETUP_ARG, StringComparison.CurrentCultureIgnoreCase))
+                    SetupTask();
+                else if (args[0].Equals(REMOVE_ARG, StringComparison.CurrentCultureIgnoreCase))
+                    RemoveTask();
+            } catch (Exception) { SetupTask(); }
         }
 
         static void UpdateWallpaper()
@@ -53,18 +56,26 @@ namespace MTG_Wallpaper_OTW
                 imageLink = htmlSource.Substring(startIndex, endIndex - startIndex);
                 client.DownloadFile(imageLink, imagePath);
             }
-
             SystemParametersInfo(0x14, 0, imagePath, 0x1 | 0x2);
         }
 
-        static void SetupTask()
+        static void SetupTask(string hr = "12", int min = 0, string meridiem = "am")
         {
+            EventLog eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("MtgWotwSource"))
+            {
+                EventLog.CreateEventSource("MtgWotwSource", "MtgWotwLog");
+            }
+            eventLog1.Source = "MtgWotwSource";
+            eventLog1.Log = "MtgWotwLog";
+            eventLog1.WriteEntry(hr + ":" + min + " " + meridiem);
+            
             TaskDefinition taskDefinition = TaskService.Instance.NewTask();
             taskDefinition.RegistrationInfo.Description = DESCRIPTION;
             // use daily in case of irregular wallpaper uploads
-            taskDefinition.Triggers.Add(new DailyTrigger { DaysInterval = 1, StartBoundary = DateTime.Today });
+            taskDefinition.Triggers.Add(new DailyTrigger { DaysInterval = 1, StartBoundary = DateTime.Parse(hr + " " + meridiem.ToUpper()).AddMinutes(min) });
             taskDefinition.Settings.StartWhenAvailable = true;
-            taskDefinition.Actions.Add(AppDomain.CurrentDomain.BaseDirectory + EXE_NAME, null, null);
+            taskDefinition.Actions.Add(System.Reflection.Assembly.GetEntryAssembly().Location, null, null);
             TaskService.Instance.RootFolder.RegisterTaskDefinition(TASK_NAME, taskDefinition);
             TaskService.Instance.FindTask(TASK_NAME).Run();
         }
